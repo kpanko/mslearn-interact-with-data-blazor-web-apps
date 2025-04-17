3ec0365e15033336f08c54dffbfc8474ecd22228
Kevin Panko <pankok@gmail.com>
2025-04-16 21:45:32 -0400

    Adding new pizza with Fixed Size

--- Model/Pizza.cs
+++ Model/Pizza.cs
@@ -25,7 +25,9 @@ namespace BlazingPizza
         public List<PizzaTopping> Toppings { get; set; } = default!;
 
         public decimal GetBasePrice() =>
-            (decimal)Size / DefaultSize * Special?.BasePrice ?? 1;
+            Special is { FixedSize: not null}
+                ? Special.BasePrice
+                : (decimal)Size / DefaultSize * Special?.BasePrice ?? 1;
 
         public decimal GetTotalPrice() => GetBasePrice();
 
